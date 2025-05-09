42d75c3e88b09af9d837965e23fa0cebea67c812
CLIM\2439764 <Aziz Souki>
2025-05-09 14:30:46 -0400

    Commit Carre 2

--- src/Partie1/Carre.java
+++ src/Partie1/Carre.java
@@ -1,63 +1,76 @@
 package Partie1;
 
-public class Carre extends Rectangle {
-    public static final int COTE_DEFAUT = 4;
+public class Carre {
+    public static final int COTE_DEFAUT = 5;
+    public static final String COUL_DEFAUT = "bleu";
+    public static final int DIM_MIN = 1;
+    public static final int DIM_MAX = 30;
+
+    private int cote;
+    private String couleur;
 
     public Carre() {
-        super(COTE_DEFAUT, COTE_DEFAUT, COUL_DEFAUT);
+        this.cote = COTE_DEFAUT;
+        this.couleur = COUL_DEFAUT;
     }
 
     public Carre(int cote, String couleur) {
-        super(cote, cote, couleur);
-        if (!validerCote(cote) || !validerCouleur(couleur)) {
-            setLargeur(COTE_DEFAUT);
-            setHauteur(COTE_DEFAUT);
-            setCouleur(COUL_DEFAUT);
+        if (validerCote(cote)) {
+            this.cote = cote;
+        } else {
+            this.cote = COTE_DEFAUT;
+        }
+
+        if (validerCouleur(couleur)) {
+            this.couleur = couleur;
+        } else {
+            this.couleur = COUL_DEFAUT;
         }
     }
 
     public int getCote() {
-        return getLargeur();
+        return cote;
     }
 
     public boolean setCote(int cote) {
         if (validerCote(cote)) {
-            super.setLargeur(cote);
-            super.setHauteur(cote);
+            this.cote = cote;
             return true;
         }
         return false;
     }
 
+    public String getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(String couleur) {
+        if (validerCouleur(couleur)) {
+            this.couleur = couleur;
+        }
+    }
+
+    public int aire() {
+        return cote * cote;
+    }
+
+    public int perimetre() {
+        return 4 * cote;
+    }
+
+    public boolean equals(Object o) {
+        if (!(o instanceof Carre)) return false;
+        Carre autre = (Carre) o;
+        return this.cote == autre.cote && this.couleur.equals(autre.couleur);
+    }
+
     public static boolean validerCote(int cote) {
-        return (cote >= DIM_MIN && cote <= DIM_MAX);
+        return cote >= DIM_MIN && cote <= DIM_MAX;
     }
 
-    @Override
-    public boolean setLargeur(int pLarg) {
-        return setCote(pLarg); // force carré rule
+    public static boolean validerCouleur(String couleur) {
+        return couleur != null &&
+                (couleur.equals("rouge") || couleur.equals("vert") ||
+                        couleur.equals("bleu") || couleur.equals("orangeRougePale"));
     }
-
-    @Override
-    public boolean setHauteur(int pHaut) {
-        return setCote(pHaut); // force carré rule
-    }
-
-    @Override
-    public String toString() {
-        return "Le côté du carré est: " + getCote() +
-                "\nLa couleur du carré est: " + getCouleur();
-    }
-
-    public static void main(String[] args) {
-        Carre carre = new Carre(6, "rouge");
-        System.out.println(carre);
-        System.out.println("Aire: " + carre.aire());
-        System.out.println("Périmètre: " + carre.perimetre());
-
-        System.out.println("\nModification du côté à 8...");
-        carre.setCote(8);
-        System.out.println(carre);
-    }
-
 }
--- src/Partie1/CarreTest.java
+++ src/Partie1/CarreTest.java
@@ -2,79 +2,108 @@ package Partie1;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CarreTest {
-
-    Carre c0, c1, c2, c3, c4, c5;
+    Carre c0, c1, c2, c3, c4, c5, c6;
 
     @BeforeEach
     public void setUp() {
         c0 = new Carre();
-        c1 = new Carre(4, "bleu");
-        c2 = new Carre(Rectangle.DIM_MIN - 1, "bleu");
-        c3 = new Carre(Rectangle.DIM_MIN, "bleu");
-        c4 = new Carre(Rectangle.DIM_MAX, "rouge");
-        c5 = new Carre(5, "couleurInvalide");
+        c1 = new Carre(7, "vert");               // valide
+        c2 = new Carre(Carre.DIM_MIN - 1, "bleu"); // côté invalide
+        c3 = new Carre(Carre.DIM_MAX + 1, "bleu"); // côté invalide
+        c4 = new Carre(8, null);                 // couleur invalide
+        c5 = new Carre(9, "ver");                // couleur invalide
+        c6 = new Carre(10, "orangeRougePale");
     }
 
     @Test
     void getCote() {
-        assertEquals(Rectangle.LARG_DEFAUT, c0.getCote());
-        assertEquals(4, c1.getCote());
-        assertEquals(Rectangle.LARG_DEFAUT, c2.getCote());
-        assertEquals(Rectangle.DIM_MIN, c3.getCote());
-        assertEquals(Rectangle.DIM_MAX, c4.getCote());
-        assertEquals(Rectangle.LARG_DEFAUT, c5.getCote());
+        assertEquals(Carre.COTE_DEFAUT, c0.getCote());
+        assertEquals(7, c1.getCote());
+        assertEquals(Carre.COTE_DEFAUT, c2.getCote());
+        assertEquals(Carre.COTE_DEFAUT, c3.getCote());
     }
 
     @Test
     void getCouleur() {
-        assertEquals(Rectangle.COUL_DEFAUT, c0.getCouleur());
-        assertEquals("bleu", c1.getCouleur());
+        assertEquals(Carre.COUL_DEFAUT, c0.getCouleur());
+        assertEquals("vert", c1.getCouleur());
         assertEquals("bleu", c2.getCouleur());
         assertEquals("bleu", c3.getCouleur());
-        assertEquals("rouge", c4.getCouleur());
-        assertEquals(Rectangle.COUL_DEFAUT, c5.getCouleur());
+        assertEquals(Carre.COUL_DEFAUT, c4.getCouleur());
+        assertEquals(Carre.COUL_DEFAUT, c5.getCouleur());
+        assertEquals("orangeRougePale", c6.getCouleur());
     }
 
     @Test
     void setCote() {
-        assertFalse(c1.setCote(Rectangle.DIM_MIN - 1));
-        assertEquals(4, c1.getCote());
-
-        assertTrue(c1.setCote(Rectangle.DIM_MIN));
-        assertEquals(Rectangle.DIM_MIN, c1.getCote());
-
-        assertTrue(c1.setCote(7));
+        assertFalse(c1.setCote(Carre.DIM_MIN - 1));
         assertEquals(7, c1.getCote());
-
-        assertTrue(c1.setCote(Rectangle.DIM_MAX));
-        assertEquals(Rectangle.DIM_MAX, c1.getCote());
-
-        assertFalse(c1.setCote(Rectangle.DIM_MAX + 1));
-        assertEquals(Rectangle.DIM_MAX, c1.getCote());
+        assertTrue(c1.setCote(Carre.DIM_MIN));
+        assertEquals(Carre.DIM_MIN, c1.getCote());
+        assertTrue(c1.setCote(10));
+        assertEquals(10, c1.getCote());
+        assertTrue(c1.setCote(Carre.DIM_MAX));
+        assertEquals(Carre.DIM_MAX, c1.getCote());
+        assertFalse(c1.setCote(Carre.DIM_MAX + 1));
+        assertEquals(Carre.DIM_MAX, c1.getCote());
     }
 
     @Test
-    void aireEtPerimetre() {
-        Carre c = new Carre(4, "bleu");
-        assertEquals(16, c.aire());
-        assertEquals(16, c.perimetre());
+    void setCouleur() {
+        c1.setCouleur(null);
+        assertEquals("vert", c1.getCouleur());
+        c1.setCouleur("ver");
+        assertEquals("vert", c1.getCouleur());
+        c1.setCouleur("rouge");
+        assertEquals("rouge", c1.getCouleur());
+        c1.setCouleur("orangeRougePale");
+        assertEquals("orangeRougePale", c1.getCouleur());
     }
 
     @Test
-    void equalsCarre() {
-        Carre cA = new Carre(5, "rouge");
-        Carre cB = new Carre(5, "rouge");
-        Carre cC = new Carre(6, "rouge");
-        Carre cD = new Carre(5, "vert");
+    void validerCote() {
+        assertFalse(Carre.validerCote(Carre.DIM_MIN - 1));
+        assertTrue(Carre.validerCote(Carre.DIM_MIN));
+        assertTrue(Carre.validerCote(15));
+        assertTrue(Carre.validerCote(Carre.DIM_MAX));
+        assertFalse(Carre.validerCote(Carre.DIM_MAX + 1));
+    }
 
-        assertEquals(cA, cB);
-        assertNotEquals(cA, cC);
-        assertNotEquals(cA, cD);
-        assertNotEquals(null, cA);
-        assertNotEquals(cA, new Rectangle(5, 5, "rouge"));
+    @Test
+    void validerCouleur() {
+        assertFalse(Carre.validerCouleur(null));
+        assertFalse(Carre.validerCouleur("rose"));
+        assertTrue(Carre.validerCouleur("rouge"));
+        assertTrue(Carre.validerCouleur("bleu"));
+        assertTrue(Carre.validerCouleur("orangeRougePale"));
+    }
+
+    @Test
+    void aire() {
+        assertEquals(49, c1.aire()); // 7 x 7
+        assertEquals(100, c6.aire()); // 10 x 10
+    }
+
+    @Test
+    void perimetre() {
+        assertEquals(28, c1.perimetre()); // 4 x 7
+        assertEquals(40, c6.perimetre()); // 4 x 10
+    }
+
+    @Test
+    void testEquals() {
+        Carre c7 = new Carre(7, "vert");
+        assertTrue(c1.equals(c7));
+        Carre c8 = new Carre(8, "vert");
+        assertFalse(c1.equals(c8));
+        Carre c9 = new Carre(7, "bleu");
+        assertFalse(c1.equals(c9));
+        assertFalse(c1.equals(null));
+        assertFalse(c1.equals("some string"));
     }
 }
 
